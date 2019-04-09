2fe6fd22f3137764f067942ff1be0a07555d8d9f
Dig <dig.vargas@alsoenergy.com>
2019-04-09 15:43:12 -0600

    adds react and react-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 console.log('My Minimal React Webpack Babel Setup');
 let abc = "def";
 console.log('abc:', abc);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
